d1a353aacd0a183b89f6ed700bf8a9442fdf428c
solarboro <boty.dh@qq.com>
2019-01-12 14:29:11 +0800

    set filter ordered

--- src/main/java/com/acn/masg/config/DefaultRootMapping.java
+++ src/main/java/com/acn/masg/config/DefaultRootMapping.java
@@ -1,6 +1,7 @@
 package com.acn.masg.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -11,6 +12,10 @@ public class DefaultRootMapping extends WebMvcConfigurerAdapter {
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/")
 		.setViewName("redirect:/masg/index.html");
+		
+		
+		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		
 		super.addViewControllers(registry);
 	}
 
